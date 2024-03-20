944a5ce515654d642744a1e4481e31d28dc46abd
PritamP20 <pripritam7@gmail.com>
2024-03-20 13:10:32 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import Category from './components/Category';
+import Navbar from './components/Navbar';
+import Seller from './components/Seller';
+import Books from './components/Books';
+import Demand from './components/Demand';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +13,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/'/>
+        <Route path='/' element={<Navbar/>}/>
+        <Route path='/' element={<Category/>}/>
+        <Route path='/' element={<Books/>}/>
+        <Route path='/' element={<Demand/>}/>
+        <Route path='/seller' element={<Seller/>}/>
+        <Route path='/' element={<Footer/>}/>
+        
       </Routes>
     </Router>
   )
